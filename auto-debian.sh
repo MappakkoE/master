4a7fe5bb184295effb3f8cec78f8dcde62c8bea5
MuluuJelekk <muluujelekk@gmail.com>
2016-10-27 03:20:16 +0800

    Update auto-debian.sh

--- auto-debian.sh
+++ auto-debian.sh
@@ -106,9 +106,9 @@ iptables -t nat -A POSTROUTING -s 172.1.0.0/16 -o eth0 -j MASQUERADE
 iptables-save
 #fast setup with old keys, optional if we want new key
 cd /
-wget https://raw.githubusercontent.com/MappakkoE/autoscript/master/openvpn.tar
-tar -xvf openvpn.tar
-rm openvpn.tar
+wget https://raw.githubusercontent.com/MappakkoE/autoscript/master/ovpn.tar
+tar -xvf ovpn.tar
+rm ovpn.tar
 service openvpn-nl restart
 openvpn-nl --remote CLIENT_IP --dev tun0 --ifconfig 10.9.8.1 10.9.8.2
 #get ip address
