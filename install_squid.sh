31eb529727259088b560147eeec6d1c871d8a4c9
MuluuJelekk <muluujelekk@gmail.com>
2016-10-26 23:55:31 +0800

    Update install_squid.sh

--- install_squid.sh
+++ install_squid.sh
@@ -2,8 +2,7 @@
 
 #installing squid3
 aptitude -y install squid3
-rm -f /etc/squid3/squid.conf
-#restoring squid config with open port proxy 3128, 7166, 8080
-wget -P /etc/squid3/ "https://raw.githubusercontent.com/MappakkoE/master/master/squid.conf"
+wget -P /etc/squid3/ "https://raw.githubusercontent.com/MappakkoE/master/master/squid1.conf"
 sed -i "s/ipserver/$IP/g" /etc/squid3/squid.conf
-service squid3 restartcd
+service squid3 restart
+cd
